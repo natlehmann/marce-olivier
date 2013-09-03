5256414b5eecce308f6f573f2ad5ddc25fd891c8
Natalia Lehmann <natlehmann@gmail.com>
2013-09-03 20:22:03 -0300

    estructura general php

---
+++ home.php
@@ -0,0 +1,15 @@
+<?php include("includes/header.php"); ?>
+
+<div id="contenido">
+	<div class="scroll">
+		<div class="galeria">
+				<img src="images/33_arcade-mode.jpg" alt="" width="573" height="800" />
+				<img src="images/33_arcade-mode.jpg" alt="" width="573" height="800" />                                	<img src="images/33_arcade-mode.jpg" alt="" width="573" height="800" />                                	<img src="images/33_arcade-mode.jpg" alt="" width="573" height="800" />                                	<img src="images/33_arcade-mode.jpg" alt="" width="573" height="800" />                                	<img src="images/33_arcade-mode.jpg" alt="" width="573" height="800" />                                	<img src="images/33_arcade-mode.jpg" alt="" width="573" height="800" />                                	<img src="images/33_arcade-mode.jpg" alt="" width="573" height="800" />                                	<img src="images/33_arcade-mode.jpg" alt="" width="573" height="800" />                                	<img src="images/33_arcade-mode.jpg" alt="" width="573" height="800" />                                	<img src="images/33_arcade-mode.jpg" alt="" width="573" height="800" />                                	<img src="images/33_arcade-mode.jpg" alt="" width="573" height="800" />                                	<img src="images/33_arcade-mode.jpg" alt="" width="573" height="800" />                                	<img src="images/33_arcade-mode.jpg" alt="" width="573" height="800" />                                	<img src="images/33_arcade-mode.jpg" alt="" width="573" height="800" />
+		</div>
+	</div>
+</div> 
+                            
+<?php 
+	$imagenFondo = "images/04.jpg";
+	include("includes/footer.php"); 
+?>
---
+++ includes/header.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es" lang="es-es">
+    <head>
+        <link rel="stylesheet" type="text/css" href="style/style.css" />
+        <script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
+        <script type="text/javascript" src="js/StackBlur.js"></script>
+        <script type="text/javascript" src="js/inicializar.js"></script>
+        <script type="text/javascript" src="js/modernizr.custom.21750.js"></script>
+    </head>
+    
+    <body>
+      <div class="container">    
+             <div id="contenedor">
+             
+             
+                   <div id="menu">
+                            <div class="container1">
+                            
+                            <a href="#"><br>
+                            <img src="images/logo.jpg" height="94" width="121"></a>
+                            
+                            <p></p>
+                            <ul>
+                            <li style="cursor: pointer;"><a href="http://www.2shywashere.com/" onclick="do_click();"> </a></li>
+                            </ul>
+                            
+                            <ul>
+                            <li style="cursor: pointer;" class="section-title">GRAPHISME</li>
+                            <li style="display: none;"><a href="http://www.2shywashere.com/index.php?/flyer/-partyharders/" onclick="do_click();">-Logo</a></li>
+                            <li style="display: none;"><a href="http://www.2shywashere.com/index.php?/zz/partyharders-night--social-club/" onclick="do_click();">-Visuel</a></li>
+                            <li style="display: none;"><a href="http://www.2shywashere.com/index.php?/flyer/-tutti-frutti/" onclick="do_click();">-Illustration</a></li>
+                            <li style="display: none;"><a href="http://www.2shywashere.com/index.php?/tee-shirt/" onclick="do_click();">-Peinture</a></li>
+                            </ul>
+                            
+                            <ul>
+                            <li style="cursor: pointer;" class="section-title">SERIGRAPHIE</li>
+                            <li class="active"><a href="http://www.2shywashere.com/index.php?/-artwork/" onclick="do_click();">-Tee-shirt</a></li>
+                            <li><a href="http://www.2shywashere.com/index.php?/logo/" onclick="do_click();">-Poster</a></li>
+                            </ul>
+                            
+                            <ul style="position: static;">
+                            <li style="cursor: pointer;" class="section-title">PHOTO</li>
+                            <li style="display: block; opacity: 0.9999;"><a href="http://www.marcelaueno.com.ar" onclick="do_click();">-Blog</a></li>
+                            </ul>
+                            
+                            <ul>
+                            <li style="cursor: pointer;" class="section-title">INFO</li>
+                            <li style="display: none;"><a href="http://www.2shywashere.com/index.php?/info/-about/" onclick="do_click();">-L'atelier</a></li>
+                            <li style="display: none;"><a href="http://www.2shywashere.com/index.php?/info/-contact/" onclick="do_click();">-Contact</a></li>
+                            <li style="display: none;"><a href="http://www.2shywashere.com/index.php?/info/-friends/" onclick="do_click();">-Amis</a></li>
+                            
+                            </ul>
+                            
+                            
+                            <!-- you must provide a link to Indexhibit on your site someplace - thank you -->
+                            <ul>
+                            <li style="cursor: pointer;"> <a href="http://www.2shywashere.com/"></a></li>
+                            </ul>
+                            
+                            </div>	
+          		</div>	                 
+                                                            
+     
+                              
+                            
