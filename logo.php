94e21e630824e2d09c57ce3a25bb6d2470c02a35
Natalia Lehmann <natlehmann@gmail.com>
2013-09-03 20:41:13 -0300

    se agregan dos paginas vacias

---
+++ logo.php
@@ -0,0 +1,10 @@
+<?php include("includes/header.php"); ?>
+
+<div id="contenido">
+
+</div> 
+                            
+<?php 
+	$imagenFondo = "images/05.jpg";
+	include("includes/footer.php"); 
+?>
---
+++ visuel.php
@@ -0,0 +1,10 @@
+<?php include("includes/header.php"); ?>
+
+<div id="contenido">
+
+</div> 
+                            
+<?php 
+	$imagenFondo = "images/33_arcade-mode.jpg";
+	include("includes/footer.php"); 
+?>
