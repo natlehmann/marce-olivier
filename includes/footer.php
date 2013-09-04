9f7d97355bf659338fb51abf27e053878495badf
Natalia Lehmann <natlehmann@gmail.com>
2013-09-03 23:14:21 -0300

    correccion de estilos para pantalla completa

--- includes/footer.php
+++ includes/footer.php
@@ -1,11 +1,12 @@
-                   <div id="bx-wrapper" class="bx-wrapper">
-                          <div class="bx-container">
-                          		<img src="<?php echo $imagenFondo ?>" alt="image01"/>
-                          </div>
-                  </div>
       		</div>
 
 		</div>
+		
+        <div id="bx-wrapper" class="bx-wrapper">
+               <div class="bx-container">
+               		<img src="<?php echo $imagenFondo ?>" alt="image01"/>
+               </div>
+       </div>
         
     </body>
 </html>
